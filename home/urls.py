9c8cb615b933f8bce52f6fa90801b188965322d1
Aniket <aniket.dg25@gmail.com>
2021-05-07 19:18:23 +0530

    Authentication App Created

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from home.views import index
+from home.views import index, productDetail
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='index'),
+    path('index', index, name='index'),
+    path('product/<product_id>/detail/', productDetail, name='productDetail'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Shoes
 from shop.settings import HOST
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def index(request):
     data = Shoes.objects.all()
     return render(request, 'index.html', { 'data':data , 'host':HOST })
 
-def productDetail(request):
-    return render(request, 'product-detail.html')
+def productDetail(request, product_id):
+    try:
+        product = Shoes.objects.get(id = int(product_id))
+        return render(request, 'product-detail.html', { 'data': product })
+    except ObjectDoesNotExist:
+        return HttpResponse("404 Page not Found")
+    
