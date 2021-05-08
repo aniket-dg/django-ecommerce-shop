a0c883662d2a9399fe730dedc713b055b00f7ad5
Aniket <aniket.dg25@gmail.com>
2021-05-08 19:34:46 +0530

    Admin Site Changes: list_display and list_filter added

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from home.models import Shoes
+from home.models import Shoes, ProductCart, Cart
 # Register your models here.
 
-admin.site.register(Shoes)
+class ShoesAdmin(admin.ModelAdmin):
+    list_display = ('name','category', 'oldPrice', 'newPrice')
+
+class ProductCartAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'user_id', 'quantity', 'total')
+    list_filter =  ('user_id',)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('productCart_id', 'cart_total')
+
+admin.site.register(Shoes, ShoesAdmin)
+admin.site.register(ProductCart, ProductCartAdmin)
+admin.site.register(Cart, CartAdmin)
+
