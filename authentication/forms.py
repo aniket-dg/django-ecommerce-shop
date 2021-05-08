51b1dee1d7e1f84c16e457752995caf27ef2b7be
Aniket <aniket.dg25@gmail.com>
2021-05-08 11:50:17 +0530

    Sign Up form added

--- authentication/forms.py
+++ authentication/forms.py
@@ -6,7 +6,7 @@ class LoginForm(forms.Form):
         label= "Enter your Username ",
     )
     password = forms.CharField(
-        max_length=5,
+        max_length=10,
     )
 
     def clean(self):
@@ -14,8 +14,35 @@ class LoginForm(forms.Form):
         username = self.cleaned_data.get('username')
         password1 = self.cleaned_data.get('password')
 
-        if username == None or len(username) < 5:
-            self._errors['username'] = self.error_class(['Minimum 5 characters required'])
+        if username == None or len(username) < 4:
+            self._errors['username'] = self.error_class(['Minimum 4 characters required'])
         if password1 == None or len(password1) < 4:
             self._errors['password'] = self.error_class(['Minimum 4 characters required'])
         return self.cleaned_data
+
+class SignUpForm(forms.Form):
+    name = forms.CharField(max_length=30, label="Full Name")
+    email = forms.EmailField(label="Your Email")
+    username = forms.CharField(min_length=4, max_length=8)
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password", max_length=8)
+    rpassword = forms.CharField(widget=forms.PasswordInput(), label="Repeat Password", max_length=8)
+
+    def clean(self):
+        super(SignUpForm, self).clean()
+        name = self.cleaned_data.get('name')
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        rpassword = self.cleaned_data.get('rpassword')
+
+        if name == None:
+            self._errors['name'] = self.error_class(['Name must not be Empty'])
+        if username is not None and len(username)<4:
+            self._errors['name'] = self.error_class(['Username must require minimum 4 characters'])
+        if username is not None and len(username)>8:
+            self._errors['username'] = self.error_class(['Username not greater then 8 characters'])
+        if password is not None and len(password) < 4:
+            self._errors['password'] = self.error_class(['Passsword length must be greater than 4'])
+        if password != rpassword:
+            self._errors['rpassword'] = self.error_class(['Password must be same'])
+        return self.cleaned_data
+
