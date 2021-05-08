3f829e7fefc4b63f3cb5df7978186073482af5b2
Aniket <aniket.dg25@gmail.com>
2021-05-08 22:18:15 +0530

    alter list_display of CartAdmin to user_id

--- home/admin.py
+++ home/admin.py
@@ -6,11 +6,11 @@ class ShoesAdmin(admin.ModelAdmin):
     list_display = ('name','category', 'oldPrice', 'newPrice')
 
 class ProductCartAdmin(admin.ModelAdmin):
-    list_display = ('product_id', 'user_id', 'quantity', 'total')
-    list_filter =  ('user_id',)
+    list_display = ('product_id', 'user_id', 'quantity', 'size', 'total')
+    list_filter =  ('user_id','product_id', 'size')
 
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('productCart_id', 'cart_total')
+    list_display = ('user_id', 'cart_total')
 
 admin.site.register(Shoes, ShoesAdmin)
 admin.site.register(ProductCart, ProductCartAdmin)
