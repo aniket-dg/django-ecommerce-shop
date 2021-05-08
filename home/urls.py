7cc47e521f232e292b27cc1472b204b4049a5d7a
Aniket <aniket.dg25@gmail.com>
2021-05-08 15:39:23 +0530

    url for /product/cart/ added

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from home.views import index, productDetail
+from home.views import index, productDetail, userCart
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='index'),
     path('index', index, name='index'),
     path('product/<product_id>/detail/', productDetail, name='productDetail'),
+    path('product/cart/', userCart, name='productCart'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
