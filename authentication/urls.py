1472bf2df925660eb76ca790838f885c49bf3bae
Aniket <aniket.dg25@gmail.com>
2021-05-08 11:53:18 +0530

    Authentication App Modified - userSignUp() method added for signup and userLogin() method improved.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import userLogin
+from .views import userLogin, userSignUp
 urlpatterns = [
     path('authenticate.do/', userLogin, name="userLogin"),
     path('', userLogin, name="userLogin"),
+    path('user/signup.do/', userSignUp, name="userSignUp"),
     
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import LoginForm
+from authentication.forms import LoginForm, SignUpForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 def userLogin(request):
     context = {}
     if request.method == "POST":
         formData = LoginForm(request.POST)
         if formData.is_valid():
-            return redirect('login.done')
+            user = authenticate(request, username= formData.cleaned_data['username'], password= formData.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/home')
+            else:
+                messages.warning(request, "Invalid Loginn Credentials")
+                return redirect('/authentication/authenticate.do/')
         else:
             context['form'] = formData
             return render(request, 'authentication/login.html', context)
     else:
         context['form'] = LoginForm(None)
         return render(request, 'authentication/login.html', context)
+
+def userSignUp(request):
+    context = {}
+    if request.method == "POST":
+        formData = SignUpForm(request.POST)
+        if formData.is_valid():
+            username = formData.cleaned_data['username']
+            first_name = formData.cleaned_data['name']
+            email = formData.cleaned_data['email']
+            password = formData.cleaned_data['password']
+            try:
+                User.objects.get(username = username, password= password)
+                messages.warning(request, "User already Registered")
+                return redirect('/authentication/authenticate.do/')
+            except ObjectDoesNotExist:
+                user = User.objects.create_user(username= username, email= email, first_name= first_name, password= password)
+                messages.success(request, "Signed Up Successfully")
+                return redirect('/authentication/authenticate.do/')
+        else:
+            context['form'] = formData
+            return render(request, 'authentication/signup.html', context)
+    else:
+        context['form'] = SignUpForm(None)
+        return render(request, 'authentication/signup.html', context)
+
