eb29a2c2c408df999625be11904869e76d96c87c
Aniket <aniket.dg25@gmail.com>
2021-05-09 12:26:01 +0530

    updateProductCart(), updatecart(), cleanCart(), cartOpearation(), change_item_to_cart() method added. messages framework imported

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from shop.settings import HOST_MEDIA
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def index(request):
     data = Shoes.objects.all()
@@ -35,8 +36,16 @@ def userCart(request):
         return redirect('/home/user/cart/')
     return render(request, 'user_cart.html')
 
+def updateProductCart(request, product_id): # update product cart using product_id
+    user_product = ProductCart.objects.get(id = product_id)
+    user_product.total = int(user_product.product_id.newPrice) * int(user_product.quantity)
+    user_product.save()
+    if user_product.quantity == 0:
+        user_product.delete()
+
 def showCart(request):
     context = {}
+    cleanCart(request)
     product_list = ProductCart.objects.filter(user_id = request.user)
     cart = Cart.objects.get(user_id = request.user)
     context['data'] = False
@@ -47,7 +56,7 @@ def showCart(request):
         context['data'] = True
     return render(request, 'user_cart.html', context)   
 
-def updateCart(request, user): #Method for update User Cart
+def updateCart(request, user): #Method for update User Cart from userCart()
     products = ProductCart.objects.filter(user_id = user)
     total = 0
     if len(products) > 0:
@@ -59,4 +68,28 @@ def updateCart(request, user): #Method for update User Cart
         user_cart = Cart(user_id = user)
     user_cart.cart_total = total
     user_cart.save()
+
+def cleanCart(request):
+    products = ProductCart.objects.filter(user_id = request.user, quantity=0).delete()
+    
+def cartOperation(request, productcart_id, op):
+    change_item_to_cart(request, productcart_id, op)
+    cleanCart(request)
+    return redirect('/home/user/cart/')
+
+def change_item_to_cart(request, productCart_id, op):
+    product = ProductCart.objects.get(id = productCart_id)
+    if op == "add":
+        if product.quantity == 10:
+            messages.warning(request,"Sorry Only 10 Quantities are allowed at one order")
+        else:
+            product.quantity += 1
+    elif op == "remove":
+        if product.quantity > 0:
+            product.quantity -=1
+    else:
+        return HttpResponse("Bad Request")
+    product.save()
+    updateProductCart(request, product.id)
+    updateCart(request, request.user)
     
