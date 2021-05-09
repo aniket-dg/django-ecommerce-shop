bee587adf9f5fce77d2d368a91738c96fad8ceb1
Aniket <aniket.dg25@gmail.com>
2021-05-09 12:03:13 +0530

    url for cartOperation() user/cart/update/<productcart_id>/<op>/ added

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import index, productDetail, userCart, showCart
+from home.views import index, productDetail, userCart, showCart, cartOperation
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('product/<product_id>/detail/', productDetail, name='productDetail'),
     path('product/cart/', userCart, name='productCart'),
     path('user/cart/', showCart, name='showCart'),
+    path('user/cart/update/<productcart_id>/<op>', cartOperation, name='cartOpeartion'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
