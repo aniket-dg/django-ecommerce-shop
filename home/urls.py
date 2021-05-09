229654e12a58c9ed123298485758e5d054ca5c45
Aniket <aniket.dg25@gmail.com>
2021-05-09 12:39:40 +0530

    url for remove_product_from_cart() user/cart/<product_id>/remove/ added

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import index, productDetail, userCart, showCart, cartOperation
+from home.views import index, productDetail, userCart, showCart, cartOperation, remove_product_from_cart
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('product/cart/', userCart, name='productCart'),
     path('user/cart/', showCart, name='showCart'),
     path('user/cart/update/<productcart_id>/<op>', cartOperation, name='cartOpeartion'),
+    path('user/cart/<product_id>/remove/', remove_product_from_cart, name='remove_product_from_cart'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
